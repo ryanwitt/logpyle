023c1142ac2bd03d1c5d7bc3a442d6b7fee3e13b
User <fastsoft@E1000-trunk-9150.(none)>
2010-03-30 15:49:18 -0700

    Checkpoint where excepthook works. Testing using syslog on freebsd.

--- logpyle/__init__.py
+++ logpyle/__init__.py
@@ -2,23 +2,47 @@
 
 import sys
 
-# Attempt to systemwide configuration
+# Attempt to import systemwide configuration
+import defaults as config
+
+# Logger identification
+import traceback
 try:
-    import logpyle_config as config
-except ImportError:
-    import defaults as config
+    name = traceback.extract_stack()[-2][0]
+except:
+    name = traceback.extract_stack()[-1][0]
+
+print '__name__', __name__
+print 'name', name
+
+from logging import *
+
+def _logger():
+    return getLogger(name)
+
+for handler, formatter in config.HANDLERS:
+    handler.setFormatter(formatter)
+    _logger().addHandler(handler)
+    getLogger().addHandler(handler)
+
+_logger().propagate = 1
+_logger().setLevel(config.LEVEL)
+getLogger().setLevel(config.LEVEL)
+
+debug = _logger().debug
+info = _logger().info
+warning = _logger().warning
+error = _logger().error
+critical = _logger().critical
+log = _logger().log
 
 # Somebody subverted the excepthook already
 if sys.excepthook != sys.__excepthook__:
     pass
 
-def excepthook(type, value, traceback):
-    sys.__excepthook__(type, value, traceback)
+def excepthook(*args):
+    _logger().error(traceback.format_exception(*args))
+    sys.__excepthook__(*args)
 
 sys.excepthook = excepthook
 
-print __name__
-
-
-def caller_module_name():
-    pass
--- logpyle/defaults.py
+++ logpyle/defaults.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+import logging
+
 LOGDIR = '/var/log/logpyle'
 
+LOGFILE = '/var/log/logpyle.log'
 
+from logging.handlers import SysLogHandler
+HANDLERS = (
+    (
+        SysLogHandler('/var/run/log', SysLogHandler.LOG_USER),
+        logging.Formatter('%(name)s %(levelname)s: %(message)s')
+    ),
+)
+
+LEVEL = logging.DEBUG
+    
+try:
+    from logpyle_config import *
+except ImportError:
+    pass
